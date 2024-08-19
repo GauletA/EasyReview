6a69ab06ecda6631b0df95be8f99be3a0fc3dbf7
Alexandre GAULET <agaulet@corp.free.fr>
2024-08-19 14:17:54 +0200

    wip color and input

--- src/components/ui/ButtonHidden/ButtonHidden.tsx
+++ src/components/ui/ButtonHidden/ButtonHidden.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react"
+import { SubmitHandler, useForm } from "react-hook-form"
 import { twMerge, twJoin } from "tailwind-merge"
 
 type Props = {
@@ -9,22 +10,68 @@ type Props = {
     str: string
 }
 
+type Inputs = {
+    value: string
+  }
+
 export default function ButtonHidden({ className, isHidden = false, str}: Props) {
     const [isHiddenState, setHiddenState] = useState(true)
-    const [isStr, setStr] = useState('')
+    const [strState, setStrState] = useState('')
+    const [isValidate, setIsValidate] = useState(false)
 
-    useEffect(() => {
+
+
+    const {
+        register,
+        handleSubmit,
+        setError,
+        reset,
+        formState: { errors },
+      } = useForm<Inputs>()
+
+      useEffect(() => {
         setHiddenState(isHidden)
-        setStr(str)
+        setStrState(str)
+        return () => {
+            reset()
+            setIsValidate(false)
+        }
     }, [str, isHidden])
 
+    const onSubmit: SubmitHandler<Inputs> = (data) => {        
+        if (data.value != str) {
+            setError('value', {type: 'validate' ,message: 'Not good!'})
+            setIsValidate(false)
+        }
+        else {
+            setHiddenState(false)
+            setIsValidate(true)
+        }
+    }
+
     return (
-        <button
-            className={twMerge("group px-3 py-1 rounded-lg border bg-blue-300 dark:bg-gray-950 border-transparent transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30", className)}
+        <div className="flex flex-col gap-2">
+                 <button
+            className={twMerge("group px-3 py-1 rounded-lg border bg-blue-300 dark:bg-gray-950 border-transparent transition-colors hover:border-green-400 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30",
+                                className,
+                                isValidate && 'bg-green-300 border-2 border-green-500',
+                                errors.value && 'bg-red-300 border-2 border-red-500')}
             onClick={() => {
                 setHiddenState(state => !state)
             }}>
-                <span className={twJoin("text-xl", isHiddenState && "opacity-0")}>{isStr}</span>
+                <span className={twJoin("text-xl", isHiddenState && "opacity-0")}>{strState}</span>
+            
         </button>
+
+        {isHidden && <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-2">
+        <input type="text" className={twJoin("rounded-md px-2", )}  {...register('value')}   />
+        </div>
+         
+        </form>}
+        
+
+        </div>
+   
     ) 
 }
